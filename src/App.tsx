912eb82690c299aaec015e6188f3714e4637260e
Sira Satheesh <satheesh2254@gmail.com>
2025-05-24 16:56:37 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app min-vh-100 d-flex flex-column">
+      <header className="py-4 bg-dark text-white text-center">
+        <h1 className="mb-0">Blink Tac Toe</h1>
+        <p className="lead mb-0">A twist on the classic with vanishing emojis</p>
+      </header>
+      <main className="flex-grow-1 py-4">
+       
+      </main>
+    </div>
+  )
+}
+
+export default App
